a04fdc6655483af7c5ca52d259cd2804f69bdb44
10725115228 <10725115228@UNS11DTPELV178.animaedu.intranet>
2025-05-22 19:53:18 -0300

    oi

--- src/main/java/com/mycompany/testegithubterminal/TesteGitHubTerminal.java
+++ src/main/java/com/mycompany/testegithubterminal/TesteGitHubTerminal.java
@@ -11,6 +11,6 @@ package com.mycompany.testegithubterminal;
 public class TesteGitHubTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("alalalacacacacac");
     }
 }
