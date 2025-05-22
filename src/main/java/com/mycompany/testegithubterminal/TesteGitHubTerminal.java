f13bc5eb5a6b971c22f12b8b2ce2ec9ec592a028
10725115625 <10725115625@UNS11DTPELV456.animaedu.intranet>
2025-05-22 19:45:39 -0300

    ksksksks

---
+++ src/main/java/com/mycompany/testegithubterminal/TesteGitHubTerminal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testegithubterminal;
+
+/**
+ *
+ * @author 10725115625
+ */
+public class TesteGitHubTerminal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
